6ca7030227c192897f0e6bf23ca3d155bbfd4e35
Daniel Ruiz <ruizg92@gmail.com>
2022-03-26 18:37:31 -0600

    [FEATURE] stream tweets working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import {useState, useEffect} from 'react';
 import { Tweet }  from './components/tweet/';
 import './App.css';
 import { Box } from '@mui/material';
 import { Layout } from './components/layout';
+import io from 'socket.io-client'
+import axios from 'axios'
+import { initSocket } from './utils/socketIO';
 
-function App() {
+const App = () =>{
+  const [tweets, setTweets] = useState([])
+  const [term, setTerm] = useState('')
+
+  useEffect(() => {
+    
+    const socket = io('http://localhost:3001/')
+    
+    socket.on('connection', () => {
+      socket.on('newTweet', (tweet) => {
+        setTweets((prevTweets) => [...prevTweets, tweet])
+      })
+      socket.on('searchTerm', (term) => {
+        setTerm({term})
+      })
+    })
+    
+    socket.on('disconnect', () => {
+      socket.off('newTweet')
+      socket.removeAllListeners('newTweet')
+    })
+  },[])
+
+  const updateSearchTerm = () => {
+    axios.post('/updateSearchTerm', {
+      searchTerm: "tesla"
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   return (
     <div className="App">
       <Layout>
@@ -13,13 +51,11 @@ function App() {
             flexDirection: 'column',
             alignItems: 'center',
         }}>
-          <Tweet />
-          <Tweet />
-          <Tweet />
-          <Tweet />
-          <Tweet />
-          <Tweet />
-          <Tweet />
+          {
+            tweets.map((tweet, index) => {
+              return <Tweet key={index} tweet={tweet} />
+            })
+          }
         </Box>
 
       </Layout>
--- src/components/tweet/Tweet.jsx
+++ src/components/tweet/Tweet.jsx
@@ -1,31 +1,35 @@
 import { Card, CardContent, CardHeader, CardMedia, Typography, Box } from "@mui/material"
 
-export const Tweet = () => {
+export const Tweet = ({ tweet }) => {
+  const { user, text } = tweet
   return (
-      <Card 
-        elevation={2}
-        gap={2}
-      >
-          <CardContent>
-              <CardHeader 
-                  title="Daniel Ruiz"
-                  subheader="@danielruiz"
-                  sx={{
-                      display: "start",
-                  }}
-              />
-            <Card sx={{maxWidth: 80, float:'right', mb:2, ml:2 }}>
-              <CardMedia 
-                  component="img"
-                  image="https://pbs.twimg.com/profile_images/1507025964534013962/90R-9xb1_400x400.jpg"
-                  alt="profile"
-                  height="80"
-              />
-            </Card>
-            <Box>
-              <Typography> hey! this is an example of a real-time tweet with Daniel #javascript #iloveElon #twiiter </Typography>
-            </Box>
-          </CardContent>
-      </Card>
+    <Card
+      elevation={2}
+      gap={2}
+      sx={{
+        width: "80%",
+      }}
+    >
+      <CardContent>
+        <CardHeader
+          title={user.name}
+          subheader={`@${user.screen_name}`}
+          sx={{
+            display: "start",
+          }}
+        />
+        <Card sx={{ maxWidth: 80, float: 'right', mb: 2, ml: 2 }}>
+          <CardMedia
+            component="img"
+            image={user.profile_image_url}
+            alt="profile"
+            height="80"
+          />
+        </Card>
+        <Box>
+          <Typography> {text} </Typography>
+        </Box>
+      </CardContent>
+    </Card>
   )
 }
