ec8fcbc6828f399437cfef423302aedc89818372
Daniel Ruiz <ruizg92@gmail.com>
2022-03-28 11:35:18 -0600

    [ENHANCEMET] refactor searchbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
 import { useEffect, useState } from 'react';
@@ -11,45 +9,8 @@ import { addTermAction } from '../../actions/tweets';
 import { tweetsApi } from '../../api'
 import { setAlertOpen } from '../../actions/ui';
 import { startLogout } from '../../actions/auth';
+import { Search, SearchIconWrapper, StyledInputBase } from './Search';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '30ch'
-    },
-  },
-}));
 
 export const Header = () => {
   const [inputValue, setInputValue] = useState('');
