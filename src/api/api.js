9b726dfda6709ffb11c865f811cc695dae1a26e1
Daniel Ruiz <ruizg92@gmail.com>
2022-03-28 12:01:03 -0600

    minor change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const tweetsApi = axios.create({
-    baseURL: 'http://localhost:3001'
+    //baseURL: 'http://localhost:3001'
+    baseURL: 'https://blackstone-challenge.netlify.app:3001'
 })
 
 
--- src/utils/socketIO.js
+++ src/utils/socketIO.js
@@ -1,3 +1,3 @@
 import io from 'socket.io-client'
 
-export const socket = io('http://localhost:3001/')
+export const socket = io('https://blackstone-challenge.netlify.app:3001')
