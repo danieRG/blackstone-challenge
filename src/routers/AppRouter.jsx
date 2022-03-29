7416a59b141acd8f06b637163874920c2552c6fd
Daniel Ruiz <ruizg92@gmail.com>
2022-03-28 19:31:14 -0600

    [ENHANCEMENT] unused lines committed

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,39 +1,37 @@
-import { useState, useEffect } from 'react'
 import {
     BrowserRouter as Router,
-    Redirect,
     Switch
 } from 'react-router-dom';
-import { useSelector } from 'react-redux'
+//import { useSelector } from 'react-redux'
 import { MainComponent } from '../components/main'
-import PrivateRoute from './PrivateRoute';
+import { Route } from 'react-router-dom';
+/* import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 import { AuthRouter } from './AuthRouter';
-import { Route } from 'react-router-dom';
-import { LoginScreen } from '../components/auth/LoginScreen';
+import { LoginScreen } from '../components/auth/LoginScreen'; */
 
 export const AppRouter = () => {
 
-    const [checking, setChecking] = useState(true)
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const { loggedIn } = useSelector(state => state.auth)
-
-    useEffect(() => {
-        let token = localStorage.getItem('token')
-
-        if (token) {
-            setIsAuthenticated(true)
-        } else {
-            setIsAuthenticated(false)
-        }
-
-        setChecking(false);
-
-    }, [loggedIn])
-
-    if (checking) {
-        return <div>Checking...</div>
-    }
+    //const [checking, setChecking] = useState(true)
+    //const [isAuthenticated, setIsAuthenticated] = useState(false)
+    //const { loggedIn } = useSelector(state => state.auth)
+    /* 
+        useEffect(() => {
+            let token = localStorage.getItem('token')
+    
+            if (token) {
+                setIsAuthenticated(true)
+            } else {
+                setIsAuthenticated(false)
+            }
+    
+            setChecking(false);
+    
+        }, [loggedIn])
+    
+        if (checking) {
+            return <div>Checking...</div>
+        } */
 
     return (
         <div>
